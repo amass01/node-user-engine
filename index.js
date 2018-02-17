3ffe4bef0f530dbf919e8d5ceab6c28589825c59
Amir Massarwi <amassarwi@gmail.com>
2018-02-17 15:04:37 +0100

    Installed jsonwebtoken, added `nodemon` start npm script, added simple root endpoint, changed port to 9191

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const morgan      = require('morgan');
 const mongoose    = require('mongoose');
 
 const jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
-const config = require('./lib/config'); // get our config file
+const config = require('./app/lib/config'); // get our config file
 const user   = require('./app/models/user'); // get our mongoose model
 
 // initiate express
 const app         = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9191;
 
 mongoose.connect(config.database); // connect to database
 app.set('superSecret', config.secret); // secret variable
@@ -20,3 +20,14 @@ app.use(bodyParser.json());
 
 // use morgan to log requests to the console
 app.use(morgan('dev'));
+
+//start the server
+app.listen(port);
+
+// basic route
+app.get('/', function(req, res) {
+    res.send('Hello! The API is at http://localhost:' + port + '/api');
+});
+
+
+console.log('Magic happens at http://localhost:' + port);
