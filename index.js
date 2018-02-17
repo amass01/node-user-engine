26f75f3ec5fbc59e2e419765c2cb8d906575dbc8
Amir Massarwi <amassarwi@gmail.com>
2018-02-17 14:54:28 +0100

    install morgan, use body parser and initiate morgan

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ const morgan      = require('morgan');
 const mongoose    = require('mongoose');
 
 const jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
-const config = require('./config'); // get our config file
+const config = require('./lib/config'); // get our config file
 const user   = require('./app/models/user'); // get our mongoose model
 
 // initiate express
 const app         = express();
+const port = process.env.PORT || 8080;
+
+mongoose.connect(config.database); // connect to database
+app.set('superSecret', config.secret); // secret variable
+
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// use morgan to log requests to the console
+app.use(morgan('dev'));
